6c6dd93c346516845959a12cdb54957a3eba28d7
AllanBernier <allan.bernier1@gmail.com>
2023-12-18 09:30:05 +0100

    Mouli es6

---
+++ mouli_es6/exercices/01_gettingStarted.js
@@ -0,0 +1,19 @@
+/**
+ * Comment me tester ? 
+ */
+
+//$> node ./gettingStarted.js
+
+function gettingStarted() {
+  return "hello world";
+}
+
+console.log(gettingStarted());
+
+
+/**
+ * Vous n'avez rien a faire sur cet exercice
+ * Vous pouvez passer a l'exercice suivant
+ * 
+ */
+module.exports = gettingStarted
